937779099fa1f2f68b3d039cdd978f1ef28f9d5d
EmanuelEscare <emaescov@gmail.com>
2022-11-24 11:19:35 -0600

    boton

--- resources/views/livewire/subir-img.blade.php
+++ resources/views/livewire/subir-img.blade.php
@@ -3,34 +3,43 @@
         <div class="m-3">
             <h3>Ayudanos a llenar este formulario para poder registrar esta ave</h3>
             <form wire:submit.prevent="save">
-            <div class="mb-3">
-                <label for="formFile" class="form-label">Subir ilustración de el ave (Dibujo o fotografía)</label>
-                <input class="form-control" type="file" wire:model="img">
-                <p> @error('img') <span class="error">{{ $message }}</span> @enderror</p>
-            </div>
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Subir ilustración de el ave (Dibujo o fotografía)</label>
+                    <input class="form-control" type="file" wire:model="img">
+                    <p> @error('img')
+                            <span class="error">{{ $message }}</span>
+                        @enderror
+                    </p>
+                </div>
 
-            <div class="mb-3">
-                <label class="form-label">Describe la ubicación y características de el ave (Ejemplo: Tamaño, Color y Hábitat)</label>
-                <textarea wire:model="info" class="form-control" rows="3"></textarea>
-            <p> @error('info') <span class="error">{{ $message }}</span> @enderror</p>  
-            </div>
-    <br>
-              <button type="submit" @disabled($this->validation) class="btn btn-primary ms-auto">Guardar</button>
+                <div class="mb-3">
+                    <label class="form-label">Describe la ubicación y características de el ave (Ejemplo: Tamaño, Color
+                        y Hábitat)</label>
+                    <textarea wire:model="info" class="form-control" rows="3"></textarea>
+                    <p> @error('info')
+                            <span class="error">{{ $message }}</span>
+                        @enderror
+                    </p>
+                </div>
+                <br>
+                <div>
+                    <button type="submit" @disabled($this->validation) class="btn btn-success ms-auto">Guardar</button>
+                    <button class="btn btn-primary" onClick="window.location.reload();">Regresar al inicio</button>
+                </div>
             </form>
+        </div>
+    </div>
+    <br>
+    @if ($this->validation)
+        <div class="card shadow border-0 bg-success text-white">
+            <div class="m-4">
+                Ave registrada
             </div>
-    </div>
-<br>
-@if ($this->validation)
-<div class="card shadow border-0 bg-success text-white">
-    <div class="m-4">
-        Ave registrada
-    </div>
-</div>
-@endif
+        </div>
+    @endif
     {{-- <script>
         window.addEventListener('notification', event => {
             $("#notification").toast('show');
         })
     </script> --}}
 </div>
-
